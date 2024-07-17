b45f9414fcc5adce0d2d9ca9b9b787781a0708ce
nahsam <ahsamn@gmail.com>
2024-07-17 18:14:58 -0500

    typo

--- NPMinstall.sh
+++ NPMinstall.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 sudo apt update
 sudo apt install nodejs
-suod apt install npm
-npm install express express-rate-limit dotenv cors body-parser access-control git multer mysql2 nedb @google-cloud/recaptcha-enterprise
+sudo apt install npm
+npm install express dotenv cors body-parser access-control git multer mysql2 nedb @google-cloud/recaptcha-enterprise
